5fd4ddc9b4734783a4f2527dc926859088f2caa8
Muhammadalif1213 <muhammadalif1213@gmail.com>
2025-04-23 20:01:54 +0700

    Menambahkan form input pesanan dengan validasi dan button untuk hitung total.

--- lib/order_page.dart
+++ lib/order_page.dart
@@ -25,6 +25,64 @@ class _OrderPageState extends State<OrderPage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final _formKey = GlobalKey<FormState>();
+    return Scaffold(
+      appBar: AppBar(title: Text('Order Page')),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          spacing: 16,
+          children: [
+            TextFormField(
+              controller: makananController,
+              decoration: const InputDecoration(labelText: 'Food Order'),
+              validator: (value){
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your food order';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: minumanController,
+              decoration: const InputDecoration(labelText: 'Drink Order'),
+              validator: (value){
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your drink order';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: jumlahMakananController,
+              decoration: const InputDecoration(labelText: 'Food QTY Order'),
+              validator: (value){
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your QTY of food order';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: jumlahMinumanController,
+              decoration: const InputDecoration(labelText: 'Drink QTY Order'),
+              validator: (value){
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your QTY of drink order';
+                }
+                return null;
+              },
+            ),
+            ElevatedButton(
+              onPressed: (){
+                if (_formKey.currentState!.validate()) {
+                  calculateTotalPrice();
+                }
+              },
+              child: Text('Order Now!'),
+            ),
+          ],
+        )),
+    );
   }
 }
