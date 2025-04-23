ce23b512eb1c538257a18b4203c1899071ded84c
Muhammadalif1213 <muhammadalif1213@gmail.com>
2025-04-23 20:43:54 +0700

    mengganti stateful menjadi statelesswidget dan menambahkan konstruktor DetailOrderPage menggunakan parameter data pesanan.

--- lib/detail_order_page.dart
+++ lib/detail_order_page.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
 
-class DetailOrderPage extends StatefulWidget {
-  const DetailOrderPage({super.key});
+class DetailOrderPage extends StatelessWidget {
+  final String makanan;
+  final String minuman;
+  final String jumlahMakanan;
+  final String jumlahMinuman;
+  final int totalHarga;
+  const DetailOrderPage({
+    super.key,
+    required this.makanan,
+    required this.minuman,
+    required this.jumlahMakanan,
+    required this.jumlahMinuman,
+    required this.totalHarga,
+  });
 
-  @override
-  State<DetailOrderPage> createState() => _DetailOrderPageState();
-}
-
-class _DetailOrderPageState extends State<DetailOrderPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+    );
   }
 }
