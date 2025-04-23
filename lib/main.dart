3e922ca2b2241dbf89f2372775e00c669feaea38
Muhammadalif1213 <muhammadalif1213@gmail.com>
2025-04-23 16:18:25 +0700

    Membuat textfield untuk inputan email.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:navigation/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,11 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const LoginPage(),
     );
   }
 }
